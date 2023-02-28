21c40c79616321813d8c0b804783c8976aa8efaf
Davy Bansal <davybansal33@gmail.com>
2023-02-28 23:22:25 +0530

    Routing added

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 import path from 'path'
 import fs from 'fs'
-
 import express from 'express'
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
+import { StaticRouter } from "react-router-dom/server";
 
 import App from '../src/App'
 
@@ -11,22 +11,48 @@ const PORT = 8080
 const app = express()
 
 const router = express.Router()
+app.use('/build', express.static('build'));
 
-const serverRenderer = (req, res, next) => {
-  fs.readFile(path.resolve('./build/index.html'), 'utf8', (err, data) => {
+// const serverRenderer = (req, res, next) => {
+//   fs.readFile(path.resolve('./build/index.html'), 'utf8', (err, data) => {
+//     if (err) {
+//       console.error(err)
+//       return res.status(500).send('An error occurred')
+//     }
+//     return res.send(
+//       data.replace(
+//         '<div id="root"></div>',
+//         `<div id="root">${ReactDOMServer.renderToString(
+//         <App/>  
+//       )}</div>`
+//       )
+//     )
+//   })
+// }
+
+app.get("*", (req, res) => {
+  let context={};
+    let html = ReactDOMServer.renderToString(
+      <StaticRouter location={req.url} context={context}>
+        {typeof window !== 'undefined' && (
+        <App/>
+      )}
+      </StaticRouter>
+    );
+    
+  const indexFile = path.resolve('./build/index.html');
+  fs.readFile(indexFile, 'utf8', (err, data) => {
     if (err) {
-      console.error(err)
-      return res.status(500).send('An error occurred')
+      console.error('Something went wrong:', err);
+      return res.status(500).send('Oops, better luck next time!');
     }
-    return res.send(
-      data.replace(
-        '<div id="root"></div>',
-        `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`
-      )
-    )
-  })
-}
-router.use('^/$', serverRenderer)
+    console.log("req.url", req.url);
+    return res.send(data.replace('<div id="root"></div>',`<div id="root">${html}</div>`));
+  });
+
+});
+
+// router.use('^/$', serverRenderer)
 
 router.use(
   express.static(path.resolve(__dirname, '..', 'build'), { maxAge: '30d' })
--- src/App.js
+++ src/App.js
@@ -1,44 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import Preview from './Components/preview';
-import { Helmet } from "react-helmet";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <Helmet>
-        <title>Test</title>
-        <meta property="og:title" content="title"></meta>
-        <meta property="og:description" content="description"></meta>
-        <meta property="og:image" content={logo}></meta>
-        {/* <meta property="og:url" content={`https://www.digrowfa.com/product/${params.id}`}></meta> */}
-        <meta property="og:image:type" content="image/png" />
-        <meta property="og:image:width" content="300" />
-        <meta property="og:image:height" content="300" />
-      </Helmet>
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <p>Davy Bansal</p>
       </header>
-      <Preview />
-    </div>
-    // <>
-    /* <Router>
-      <Switch>
-        <Route path="/preview" exact component={Preview}/>
-      </Switch>
-    </Router> */
-    // </>
+      <Router>
+        <Routes>
+          <Route exact path="/preview" element={<Preview />} />
+        </Routes>
+      </Router>
+    </> 
   );
 }
 
