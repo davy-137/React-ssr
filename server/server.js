52de900333e3d5a8df526663d8d76d84b45af613
Davy Bansal <davybansal33@gmail.com>
2023-03-01 00:57:05 +0530

    Add server side rendering

--- server/server.js
+++ server/server.js
@@ -1,67 +1,19 @@
 import path from 'path'
-import fs from 'fs'
 import express from 'express'
-import React from 'react'
-import ReactDOMServer from 'react-dom/server'
-import { StaticRouter } from "react-router-dom/server";
-
-import App from '../src/App'
 
 const PORT = 8080
 const app = express()
 
 const router = express.Router()
-app.use('/build', express.static('build'));
 
-// const serverRenderer = (req, res, next) => {
-//   fs.readFile(path.resolve('./build/index.html'), 'utf8', (err, data) => {
-//     if (err) {
-//       console.error(err)
-//       return res.status(500).send('An error occurred')
-//     }
-//     return res.send(
-//       data.replace(
-//         '<div id="root"></div>',
-//         `<div id="root">${ReactDOMServer.renderToString(
-//         <App/>  
-//       )}</div>`
-//       )
-//     )
-//   })
-// }
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get("*", (req, res) => {
-  let context={};
-    let html = ReactDOMServer.renderToString(
-      <StaticRouter location={req.url} context={context}>
-        {typeof window !== 'undefined' && (
-        <App/>
-      )}
-      </StaticRouter>
-    );
-    
-  const indexFile = path.resolve('./build/index.html');
-  fs.readFile(indexFile, 'utf8', (err, data) => {
-    if (err) {
-      console.error('Something went wrong:', err);
-      return res.status(500).send('Oops, better luck next time!');
-    }
-    console.log("req.url", req.url);
-    return res.send(data.replace('<div id="root"></div>',`<div id="root">${html}</div>`));
-  });
-
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
-// router.use('^/$', serverRenderer)
-
-router.use(
-  express.static(path.resolve(__dirname, '..', 'build'), { maxAge: '30d' })
-)
-
-// tell the app to use the above rules
 app.use(router)
 
-// app.use(express.static('./build'))
 app.listen(PORT, () => {
   console.log(`SSR running on port ${PORT}`)
 })
--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       </header>
       <Router>
         <Routes>
-          <Route exact path="/preview" element={<Preview />} />
+          <Route exact path='/preview' element={<Preview />} />
         </Routes>
       </Router>
     </> 
