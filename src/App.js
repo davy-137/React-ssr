3315c7283c5d9d3074b6faab60799058df91d063
Davy Bansal <davybansal33@gmail.com>
2023-02-28 11:52:56 +0530

    Testing purpose

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import Preview from './Components/preview';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Helmet } from "react-helmet";
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>Test</title>
+        <meta property="og:title" content="title"></meta>
+        <meta property="og:description" content="description"></meta>
+        <meta property="og:image" content={logo}></meta>
+        {/* <meta property="og:url" content={`https://www.digrowfa.com/product/${params.id}`}></meta> */}
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="300" />
+        <meta property="og:image:height" content="300" />
+      </Helmet>
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
@@ -18,7 +31,15 @@ function App() {
           Learn React
         </a>
       </header>
+      <Preview />
     </div>
+    // <>
+    /* <Router>
+      <Switch>
+        <Route path="/preview" exact component={Preview}/>
+      </Switch>
+    </Router> */
+    // </>
   );
 }
 
---
+++ src/Components/preview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Preview = () => {
+    return(
+        <>
+            <p>Testing</p>
+        </>
+    )
+}
+
+export default Preview;
